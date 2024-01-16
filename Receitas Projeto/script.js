b42a6ab0cdf78ac27cb42fa2049ae5d0088e339c
Thiago_trx <70294081+Thiagobr19@users.noreply.github.com>
2024-01-15 21:07:52 -0300

    Add files via upload

---
+++ Receitas Projeto/script.js
@@ -0,0 +1,6 @@
+function submitForm() {
+    // Aqui você pode adicionar a lógica para processar o formulário
+    var form = document.getElementById("recipeForm");
+    alert("Formulário enviado com sucesso!");
+    form.reset();
+}
